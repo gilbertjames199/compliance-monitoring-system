8e65074c644308862aed26332c401b3fd9650928
Keyem04 <krstnmvrgs04@gmail.com>
2026-03-30 09:01:34 +0800

    for printing

--- resources/views/filament/print.blade.php
+++ resources/views/filament/print.blade.php
@@ -1,6 +1,7 @@
 <iframe 
-    src="http://paps.davaodeoro.gov.ph/jasperserver/flow.html?pp=u%3DJamshasadid%7Cr%3DManager%7Co%3DEMEA,Sales%7Cpa1%3DSweden&_flowId=viewReportFlow&_flowId=viewReportFlow&_flowId=viewReportFlow&ParentFolderUri=%2Freports&reportUnit=%2Freports%2FcomplianceReport&standAlone=true&decorate=no&output=pdf&requirement_id={{ urlencode($requirement_id) }}" 
+    src="https://paps.davaodeoro.gov.ph/jasperserver/flow.html?pp=u%3DJamshasadid%7Cr%3DManager%7Co%3DEMEA,Sales%7Cpa1%3DSweden&_flowId=viewReportFlow&_flowId=viewReportFlow&_flowId=viewReportFlow&ParentFolderUri=%2Freports&reportUnit=%2Freports%2FcomplianceReport&standAlone=true&decorate=no&output=pdf&requirement_id={{ urlencode($requirement_id) }}" 
     frameborder="0"
     width="100%" 
     height="800px" >
 </iframe>
+
